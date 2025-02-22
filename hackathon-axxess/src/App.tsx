1ca0b637ef785da12ad30c769cb02948fdd8344f
y0ye <bennettoppenheimer@gmail.com>
2025-02-22 16:24:32 -0600

    Basic dashboard complete

--- hackathon-axxess/src/App.tsx
+++ hackathon-axxess/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="dashboard">
+      <div className='left'>
+        <div className='widget'>
+          <button>Health Tracker</button>
+        </div>
+        <div className='widget'>
+        <button>AI Summary</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='right'>
+        <div className='widget'>
+        <button>Medical Information</button>
+        </div>
+        <div className='widget'>
+          <button>What Did I Miss?</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
