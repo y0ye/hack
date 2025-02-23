afde3b8379e1375fc2ef4a168bb7a6c6927d9aaf
zhaotongzhang <zxz220016@utdallas.edu>
2025-02-22 22:36:07 -0600

    db work

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
+// Routers
+
+// get data
+app.get("/paitent", async(req, res) => {
+    try{
+        const allpaitent = await pool.query("SELECT * FROM paitent WHERE patient_id = 1");
+        res.json(allpaitent.rows);
+    } catch (err) {
+        console.erro(err.message)
+    }
+}
+)
+
 app.listen(5001, () => {
-    console.log("server has started on port 5000");
+    console.log("server has started on port 5001");
 });
