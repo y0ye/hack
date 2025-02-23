8d6602adf8d2eb5691b3e202c7c34c0f0f53abc5
Bennett Oppenheimer <obennett66@gmail.com>
2025-02-22 23:51:37 -0600

    modifying layout

--- hackathon-axxess/src/components/MedicalData.tsx
+++ hackathon-axxess/src/components/MedicalData.tsx
@@ -10,23 +10,10 @@ export default function MedicalData() {
             <div className={classes.pills}>
                 <Pill />
                 <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
-                <Pill />
             </div>
             <div className={classes.appointmentdata}>
                 <form>
-                    <h1 className={classes.appointmentHeader}>Most Recent Appointment</h1>
+                    <h1 className={classes.appointmentHeader}>Appointment</h1>
                     <h2 className={classes.appointType}>Appointment Type</h2>
                     <h3>Appointment Date</h3>
                     <h1>Status</h1>
